718031856296fdc3d48d6573b6cca9558c6db7c4
nikolajtrusnikov <niktru95@outlook.com>
2022-10-03 12:15:01 +0300

    Delete some parameters

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,9 @@ import pytest
 
 @pytest.fixture()
 def set_up_tear_down(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
-    # page.set_viewport_size({'width': 1080, 'height': 1920})
     page.goto('https://www.saucedemo.com/')
     yield page
     context.close()
